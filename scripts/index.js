a6ac1cc2867622b6c9ebcf29de39b69cebda3037
Rachel N <software.engineer.rachel@gmail.com>
2023-12-15 21:51:14 -0600

    Stage 3 1st upload

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,9 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+const profileEditButton = document.querySelector(".profile__arrow");
+const profileEditModal = document.querySelector(".modal");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal__opened");
+});
