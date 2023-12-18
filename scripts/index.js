46629211def880b3fd209c76abcd89dcbfa3d04e
Rachel N <software.engineer.rachel@gmail.com>
2023-12-18 16:20:38 -0600

    Stage 4 1st upload

--- scripts/index.js
+++ scripts/index.js
@@ -26,7 +26,51 @@ const initialCards = [
 ];
 const profileEditButton = document.querySelector(".profile__arrow");
 const profileEditModal = document.querySelector(".modal");
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__subheading");
+
+const modalInputName = document.querySelector("#modal__input-name");
+const modalInputDescription = document.querySelector(
+  "#modal__input-description"
+);
+
+const profileModalClose = document.querySelector(".modal__close");
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+  const cardListEl= document.querySelector('.card-wrapper');
+
+function closePopup() {
+  profileEditModal.classList.remove("modal__opened");
+}
+
+function getCardElement(cardData){
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector('.card__image');
+  const cardTitleEl = cardElement.querySelector('.card__title');
+  cardTitleEl.textContent = cardData.name;
+  cardImageEl.textContent = cardData.link;
+  return cardElement;
+}
 
 profileEditButton.addEventListener("click", () => {
+  modalInputName.value = profileName.textContent;
+  modalInputDescription.value = profileDescription.textContent;
   profileEditModal.classList.add("modal__opened");
 });
+
+profileModalClose.addEventListener("click", () => {
+  closePopup();
+});
+
+profileEditForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  profileName.textContent = modalInputName.value;
+  profileDescription.textContent = modalInputDescription.value;
+  closePopup();
+});
+
+initialCards.forEach(cardData) {
+const cardElement = getCardElement(cardData);
+cardListEl.prepend(cardElement);
+}
